06e7fb0a7513164183c30425f2194d4834e4f65b
grostaco <grostaco@gmail.com>
2023-01-18 00:59:37 +0700

    feat: more skeleton files

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use crate::math::{GaussElimIter, Mat2d};
 
 mod components;
 mod math;
+mod routes;
 
 fn main() {
-    let mat = Mat2d::from([[0., 2., 1., 1.], [3., -7., -6., 1.], [0., -1., -1., 1.]]);
+    let mat = Mat2d::from([
+        [0., 2., 1., 1.],
+        [3., -7., -6., 1.],
+        [0., -1., -1., 1.],
+        [2., 3., 4., 5.],
+    ]);
     let g = GaussElimIter::from(mat);
     println!("{:#?}", g.collect::<Vec<_>>());
 }
