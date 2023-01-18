c08c76aeaf3e41798e04249dcec9882d0652fff1
grostaco <grostaco@gmail.com>
2023-01-18 18:58:32 +0700

    refactor: mod

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,8 +2,12 @@ mod dimension_input;
 mod display;
 mod input;
 mod mat;
+mod mat_edit;
 mod nav;
+mod steps;
 
 pub use dimension_input::DimensionInput;
 pub use mat::Mat;
+pub use mat_edit::MatEdit;
 pub use nav::Nav;
+pub use steps::Steps;
